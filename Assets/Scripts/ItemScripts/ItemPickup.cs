b534aa6727617d1e7958ba4ca5a5f06e171c394f
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-07-02 16:28:31 -0500

    item animation play on destruction now functional

--- Assets/Scripts/ItemScripts/ItemPickup.cs
+++ Assets/Scripts/ItemScripts/ItemPickup.cs
@@ -6,26 +6,34 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+
+    Animator itemAnimator;
+
     public float PickUpRadius = 1f;
     public InventoryItemData ItemData;
 
     CircleCollider2D itemCollider;
 
     private void Awake() {
+        itemAnimator = GetComponent<Animator>();
         itemCollider = GetComponent<CircleCollider2D>();    
         itemCollider.isTrigger = true;
         itemCollider.radius = PickUpRadius;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        
         var inventory = other.transform.GetComponent<InventoryHolder>();
-
         if(!inventory) {
             return;
         }
 
         if(inventory.InventorySystem.AddToInventory(ItemData, 1)) {
-            Destroy(this.gameObject);
+            itemAnimator.Play("MagicAbsorb");
+            Destroy(this.gameObject, itemAnimator.GetCurrentAnimatorStateInfo(0).length);
+
         }
     }
+
 }
+
